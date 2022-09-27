e2bf77b6411d8f9a199ccd516064a4e57a955dec
Nick M <mully7773@gmail.com>
2022-09-27 10:38:45 -0500

    Add state to model for advice and ids

--- src/js/model.js
+++ src/js/model.js
@@ -1,14 +1,25 @@
 import { ADVICE_API_URL } from './config.js';
 import { getJSON } from './helpers.js';
 
+export const state = {
+  advice: {},
+  id: {},
+};
+
 export const loadAdvice = async () => {
   try {
     const adviceData = await getJSON(`${ADVICE_API_URL}`);
     console.log(adviceData);
+
+    const { slip } = adviceData;
+    console.log(slip);
+
+    state.advice = slip.advice;
+    state.id = slip.id;
+    console.log(state.advice);
+    console.log(state.id);
   } catch (err) {
     console.error(`Fetch problem: ${err.message}`);
     throw err;
   }
 };
-
-loadAdvice();
